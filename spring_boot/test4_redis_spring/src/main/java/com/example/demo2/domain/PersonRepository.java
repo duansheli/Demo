80ccbef78cd32ed15762ba9a94bbdc7438b93c52
graft <1016929439@qq.com>
2018-12-30 20:33:21 +0800

    移除redis示例

--- spring_boot/test4_redis_spring/src/main/java/com/example/demo2/domain/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo2.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, String> {
-
-}
--- spring_boot/test4_redis_spring/src/main/java/com/example/demo3/domain/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo3.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, String> {
-
-}
