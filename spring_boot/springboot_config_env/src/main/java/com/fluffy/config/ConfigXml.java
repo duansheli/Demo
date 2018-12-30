8293d9521920380ecbc9654ded983046fd39df46
graft <1016929439@qq.com>
2018-12-31 00:55:52 +0800

    移除sping环境配置-项目

--- spring_boot/springboot_config_env/src/main/java/com/fluffy/config/ConfigXml.java
+++
@@ -1,9 +0,0 @@
-package com.fluffy.config;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource(locations={"classpath:spring-demo.xml"})
-public class ConfigXml {
-    
-}
